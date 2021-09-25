07edf7e9a91d3165ac576cc39302cf3c1b2fc1bc
kowais915 <kowais915@gmail.com>
2021-09-25 11:52:28 +0500

    set up the temp

---
+++ app.js
@@ -0,0 +1,11 @@
+// const getRick = async()=>{
+
+
+//     const base  = "https://rickandmortyapi.com/api/character/1";
+//     const response = await fetch(base);
+//     const data = await response.json();
+
+
+//     console.log(data);
+//     return data;
+// }
