e7e7d35e6d979da4c606d07a0cd179058ab2f631
kowais915 <kowais915@gmail.com>
2021-09-26 15:28:27 +0500

    fixed a bug. The form works. Prevented the default refreshing of the form

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 
-const name = document.getElementById('name');
-const status = document.getElementById('status');
+const nameChar = document.getElementById('name');
+const statusChar = document.getElementById('status');
 const loc = document.getElementById('location');
 const gender = document.getElementById('gender');
 const image = document.getElementById('image');
 
 
 
+const search = document.querySelector('form');
+
+search.addEventListener('submit', (e)=>{
+
+    const user = search.character.value.trim(); 
+
+    getChar(user).then(data=>{
+
+       
+        console.log(data);
+        
+        nameChar.innerText += " " + data.name;
+        statusChar.innerText += " " + data.status;
+        loc.innerText += " " + data.location.name;
+        gender.innerText += " " + data.gender;
+        image.setAttribute("src", data.image);
+        
+        
+        
+        }).catch(err=>{
+            console.log(err);
+        });
+
+        e.preventDefault();
+
+       
+})
 
 
-const data = prompt("Enter name: ");
-
-getChar(data).then(data=>{
-
-console.log(data);
-
-name.innerText += " " + data.name;
-status.innerText += " " + data.status;
-loc.innerText += " " + data.location.name;
-gender.innerText += " " + data.gender;
-image.setAttribute("src", data.image);
 
 
 
-}).catch(err=>{
-    console.log(err);
-});
+// const data = prompt("Enter name: ");
+
